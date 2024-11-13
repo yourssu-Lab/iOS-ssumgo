fb0115f5f5506b89e68ab852fdb08bf25f8ecbd5
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-11-13 13:53:04 +0900

    [Setting] xcode 파일 세팅

---
+++ iOS-ssumgo/iOS-ssumgo/Application/iOS_ssumgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ssumgoApp.swift
+//  iOS-ssumgo
+//
+//  Created by 정민지 on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ssumgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ iOS-ssumgo/iOS-ssumgo/Source/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  iOS-ssumgo
+//
+//  Created by 정민지 on 11/13/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
