d227edb7bb3b5d481914b5bae653c9e6949fffa0
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-11-22 11:01:25 +0900

    [Feat] URLSession을 사용한 네트워크 세팅

---
+++ iOS-ssumgo/iOS-ssumgo/Source/Network/Base/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  iOS-ssumgo
+//
+//  Created by 정민지 on 11/22/24.
+//
+
+// MARK: - HTTPMethod
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
